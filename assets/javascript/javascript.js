1f60758ca563edddd5a908209a5120416a008ce7
stephanie <stephanie99tech@gmail.com>
2018-10-07 15:20:24 -0700

    updated code

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,6 +1,6 @@
 
 
-var topics = ["cotton candy", "friends", "bears", "balloons", "stuffed animals"]; // array of items from carnival 
+var topics = [ "balloons", "stuffed animals", "bears", "cotton candy", "friends"]; // array of items from carnival 
         // that we will create buttons for and add to html page
    
     var buttonArray; // this creates the buttons from the 'topics' array above.
@@ -11,62 +11,30 @@ var topics = ["cotton candy", "friends", "bears", "balloons", "stuffed animals"]
 
         select();
 
-    //     $("#additem").on("click", function() { // Event listener for our existing buttons 
-            
-    //     var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + topics[index]; 
-    //         // Storing our giphy API URL for a random 1st image (bunny) for example
-    //     $.ajax ({ // Perfoming an AJAX GET request to our queryURL
-    //         url: queryURL,
-    //         method: "GET"
-    //     })
-    //     .then(function(response) {  // After the data from the AJAX request comes back
-    //         console.log(response);
-    //         $('#additem').text(JSON.stringify(response.rating));
-    //                 console.log(response.rating);
-
-
-    //         var rate_rating = $("<h3>"); //  5. Under every gif, display its rating (PG, G, so on).
-    //         rate_rating.text("Rating: " + response.rating);
-            
-    //         var imageUrl = response.data.image_original_url; // Saving the image_original_url property            
-    //         var button1 = $("<img>");  // Creating and storing an image element
-    //         button1.attr("src", imageUrl); // Setting the button_1 src attribute to imageUrl
-    //         button1.attr("alt", "first image");
-        
-    //         $("#images1").empty();
-    //         $("#images1").prepend(button1).prepend(rate_rating); // Prepending the button_1 to the images div
-    //         });
-    //     });   
     // ====== leave code above alone this is for the array of buttons printed to screen ======
+
             function select() {
                  $("#additem").on("click", function() { // Event listener for our existing buttons 
             
         var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + topics[index]; 
             // Storing our giphy API URL for a random 1st image (bunny) for example
+
         $.ajax ({ // Perfoming an AJAX GET request to our queryURL
             url: queryURL,
             method: "GET"
-        })
-        .then(function(response) {  // After the data from the AJAX request comes back
+        }).then(function(response) { 
             console.log(response);
-            $('#additem').text(JSON.stringify(response.data.rating));
-                    console.log(response.rating);
 
-
-            var rate_rating = $("<h3>"); //  5. Under every gif, display its rating (PG, G, so on).
-            rate_rating.text("Rating: " + response.data.rating);
-            
             var imageUrl = response.data.image_original_url; // Saving the image_original_url property            
             var button1 = $("<img>");  // Creating and storing an image element
             button1.attr("src", imageUrl); // Setting the button_1 src attribute to imageUrl
             button1.attr("alt", "first image");
         
             $("#images1").empty();
-            $("#images1").prepend(button1).prepend(rate_rating); // Prepending the button_1 to the images div
+            $("#images1").prepend(button1); // Prepending the button_1 to the images div
             });
         });
             } // end of function select
-
 
             function still_animate() {
                  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element.
@@ -101,20 +69,15 @@ var topics = ["cotton candy", "friends", "bears", "balloons", "stuffed animals"]
             .then(function(response) {
                 console.log(response);
 
-            $('#moreitems').text(JSON.stringify(response.data.rating));
-                console.log(response.data.rating);
-            var rate_rating = $("<h3>"); //  5. Under every gif, display its rating (PG, G, so on).
-            rate_rating.text("Rating: " + response.data.rating);
-
             var imageUrl = response.data.image_original_url; // Saving the image_original_url property
             var button1 = $("<img>"); // Creating and storing an image element
-            //button1.addClass("newbutton");
+          
             $("button").addClass("newbutton");
             button1.attr("src", imageUrl); // Setting the button1 src attribute to imageUrl
             button1.attr("alt", "first image");
 
             $("#images1").empty();
-            $("#images1").prepend(button1).prepend(rate_rating);  // Prepending the button1 to the images1 div
+            $("#images1").prepend(button1);  // Prepending the button1 to the images1 div
       
             newbutton2()
 
@@ -131,19 +94,14 @@ function newbutton2() {
             .then(function(response) {
                 console.log(response);
 
-            $('#moreitems').text(JSON.stringify(response.data.rating));
-                console.log(response.data.rating);
-                var rate_rating = $("<h3>"); //  5. Under every gif, display its rating (PG, G, so on).
-                rate_rating.text("Rating: " + response.data.rating);
-
             var imageUrl = response.data.image_original_url; // Saving the image_original_url property
             var newbuttonphoto = $("<img>"); // Creating and storing an image element
-            //button2.addClass("newbutton");
-           newbuttonphoto.attr("src", imageUrl); // Setting the button1 src attribute to imageUrl
+           
+            newbuttonphoto.attr("src", imageUrl); // Setting the button1 src attribute to imageUrl
             newbuttonphoto.attr("alt", "next image");
 
                 // $("#images2").empty();
-                 $("#images1").prepend(newbuttonphoto).prepend(rate_rating);;
+                 $("#images1").prepend(newbuttonphoto);
 
             });
         });  // end of .newbutton
@@ -151,6 +109,3 @@ function newbutton2() {
     } // end of newbutton2()
 
 
-// 3. When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page. 
-
-// 4. When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
